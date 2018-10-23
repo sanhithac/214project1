cd6fd8b388e2a28ebedc94661e87aa466ed8f530
keyapatel <36779143+keyapatel@users.noreply.github.com>
2018-10-22 21:57:36 -0400

    Update scannerCSVsorter.c

--- scannerCSVsorter.c
+++ scannerCSVsorter.c
@@ -1,41 +1,41 @@
-/* #include <dirent.h> */
-/* #include <stdio.h> */
-/* #include <stdlib.h> */
-/* #include <string.h> */
-/* #include <sys/types.h> */
+#include <dirent.h> 
+ #include <stdio.h> 
+ #include <stdlib.h> 
+ #include <string.h> 
+ #include <sys/types.h> 
 
-/* //#include "header.h" */
-/* //#include "movies.c" */
-/* //#include "mergesort.c" */
+//#include "header.h" 
+//#include "movies.c" 
+//#include "mergesort.c" 
 
-/* int main(int argc, char** argv){ */
-/*   if(argc%2 ==0){ */
-/*     return -1; */
-/*   } */
-/*   if(argc >7){ */
-/*     return -1; */
-/*   } */
-/*   if(argv[1] != "-c"){ */
-/*     return -1; */
-/*   } */
-/*   if(argv[2] == NULL){ */
-/*     return -1;  */
-/*   } */
+ int main(int argc, char** argv){ 
+   if(argc%2 ==0){ 
+     return -1; 
+   } 
+   if(argc >7){ 
+     return -1; 
+   } 
+   if(argv[1] != "-c"){ 
+     return -1; 
+   } 
+   if(argv[2] == NULL){ 
+     return -1;  
+   } 
   
-/*   DIR* dir; */
-/*   struct dirent* ptr; */
+   DIR* dir; 
+   struct dirent* ptr; 
 
-/*   dir = opendir("project1"); */
-/*   if(dir == NULL){ */
-/*     return -1; */
-/*   } */
+   dir = opendir("project1"); 
+   if(dir == NULL){ 
+     return -1; 
+   } 
  
-/*   while((ptr = readdir(dir)) != NULL){ */
-/*     //fork */
-/*     //while */
-/*     //new method: if ptr->d_name is csv,return 1. if direc, return 0. else return -1 */
-/*     //if csv,   */
-/*   } */
+   while((ptr = readdir(dir)) != NULL){ 
+     //fork 
+     //while 
+     //new method: if ptr->d_name is csv,return 1. if direc, return 0. else return -1 
+     //if csv,   
+  } 
 
  /*
 		void directChild(DIR* dir = opendir(dirc1)){
@@ -62,5 +62,5 @@
 			}
 		}
 	*/
-/*   return 0; */
-/* } */
+   return 0; 
+ } 
